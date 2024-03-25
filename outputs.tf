5011cab3795dfb12ef34825cc7a2c75b7cfc801c
Srikanth <srikanth2@hp.com>
2024-03-25 11:03:01 +0100

    Added the most usable output the module

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "key_pair"{
 output "oidc_provider_arn"{
     value = module.eks.oidc_provider_arn
 }
+
+output "cluster_endpoint"{
+    value = module.eks.cluster_endpoint
+}
+
+output "cluster_version"{
+    value = module.eks.cluster_version
+}
+
+output "cluster_id"{
+    value = module.eks.cluster_id
+}
